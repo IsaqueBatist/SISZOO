1c84fa757fe68c8c0499c3a488cb960160f41113
Isaque Barbosa <isaquebatista400@gmail.com>
2026-08-22 15:42:09 -0300

    Instalando MSW e configurando testes

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom/vitest'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+import { server } from '../mocks/server'
+
+// TODO: migrar para 'error' quando as integrações reais de API existirem,
+// para acusar chamadas HTTP não-mockadas nos testes.
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }))
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
